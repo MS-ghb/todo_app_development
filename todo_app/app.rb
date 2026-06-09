7d81bcddad81e23927c8221b4878a46a02b6eb67
Moe Saito <s_shigi_foxtail2323_96pon@yahoo.co.jp>
2026-06-09 21:36:45 +0900

    Add todo_app files

---
+++ todo_app/app.rb
@@ -0,0 +1,70 @@
+require 'sinatra'
+require 'active_record'
+
+use Rack::MethodOverride
+
+ActiveRecord::Base.establish_connection(
+    adapter:  "mysql2",
+    host:     "localhost",
+    username: "root",          # Windowsの標準ユーザー名
+    password: "mysql2023", # 【重要】MySQLインストール時に決めたパスワードを入力
+    database: "todo_app_development"
+)
+#AcriveRecord タイムゾーン指定
+ActiveRecord.default_timezone = :local  
+
+class Todo < ActiveRecord::Base; end
+
+get '/' do
+  @todos = Todo.all
+  erb :index
+end
+
+post '/' do
+  @newrecord = Todo.new
+  @newrecord.title = params[:title]
+  @newrecord.description = params[:description]
+  # completedの初期値は0(未完了)
+  @newrecord.completed = 0
+  # saveでtodosテーブルに保存
+  @newrecord.save   
+  # index画面（トップページ）へURLごと移動
+  redirect '/' 
+end
+
+get '/:id/edit' do
+  @record = Todo.find_by_id(params[:id]) #該当IDのデータ取得
+  erb :edit # views/edit.erb を表示
+end
+
+# 編集画面での更新処理を反映
+patch '/:id' do
+	@record = Todo.find_by_id(params[:id]) #該当IDのデータ情報取得
+  #更新データの代入
+	@record.title = params[:title] 
+  @record.description = params[:description]
+	@record.save #データ更新の反映
+	redirect to "/" #一覧画面へ
+end
+# 完了・未完了切り替え（トグルボタン）
+patch '/:id/toggle' do
+	@record = Todo.find_by_id(params[:id]) #該当IDのデータ情報取得
+  # 現在の値が 1 または true なら 0 に更新、それ以外は 1 に更新
+  if @record.completed == 1 || @record.completed == true then
+    @record.completed = 0
+  else
+    @record.completed = 1
+  end
+	@record.save #データ更新の反映
+	redirect to "/" #一覧画面へ
+end
+
+delete '/:id' do
+  id = params[:id]
+    Todo.destroy(id)
+    redirect '/'
+end
+
+
+
+
